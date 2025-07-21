b233aa7ff1e9fdb782cd0694cc6ee6b607996252
NikolaDX <nikolar2903@gmail.com>
2025-07-21 23:16:44 +0200

    Implementirani osnovni video pozivi

--- lib/pages/video_call/main_page.dart
+++ lib/pages/video_call/main_page.dart
@@ -1,11 +1,40 @@
-
 import 'package:flutter/material.dart';
+import 'package:proxima/classes/models/user.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:proxima/zego_ui_options.dart';
+
+final User exampleUser1 = User(
+  id: 'cc-5679-zz',
+  locationX: 57.35009,
+  locationY: 13.74389,
+  name: 'Goran',
+  surname: 'Edman',
+  interests: ['Music', 'Singing'],
+);
+
+final User exampleUser2 = User(
+  id: '34560-c4',
+  locationX: 35.679591,
+  locationY: 139.770380,
+  name: 'Ryota',
+  surname: 'Hayami',
+  interests: ['Music', 'Singing', 'Guitar'],
+);
+
+final String exampleCallId = 'c3-b6';
 
 class VideoCallMainPage extends StatelessWidget {
   const VideoCallMainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ZegoUIKitPrebuiltCall(
+      appID: ZegoUIOptions.currentOptions.appId,
+      callID: exampleCallId,
+      userID: exampleUser1.id ?? '3565',
+      userName: exampleUser1.fullName,
+      config: ZegoUIKitPrebuiltCallConfig(turnOnCameraWhenJoining: false),
+      appSign: ZegoUIOptions.currentOptions.appSign,
+    );
   }
 }
---
+++ lib/zego_ui_options.dart
@@ -0,0 +1,11 @@
+class ZegoUIOptions {
+  int appId;
+  String appSign;
+
+  ZegoUIOptions({required this.appId, required this.appSign});
+
+  static final ZegoUIOptions currentOptions = ZegoUIOptions(
+    appId: 1610318128,
+    appSign: 'cf0594b9efac9b384bbb830f9e1acfdf40f606ce5be9e3bb16c8648af37d7e8f',
+  );
+}
